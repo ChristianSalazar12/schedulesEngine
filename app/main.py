5d163390dbdf15ea4435831d852e115ea5582650
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-07-02 13:59:13 -0500

    project create and structure

---
+++ app/main.py
@@ -0,0 +1,17 @@
+# from openai import OpenAI
+
+# client = OpenAI(
+#   api_key="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"
+# )
+
+# completion = client.chat.completions.create(
+#   model="gpt-4o-mini",
+#   store=True,
+#   messages=[
+#     {"role": "user", "content": "write a haiku about ai"}
+#   ]
+# )
+
+# print(completion.choices[0].message);
+
+##this is my key : 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
